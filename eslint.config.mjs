e293bc5e3ed98f3c46cf4b5ae2913ed67ada32ad
NICKALL-DESKTOP\User <nick0918964388@gmail.com>
2025-05-12 11:26:32 +0800

    更新 ESLint 配置，新增關閉 @typescript-eslint/ban-ts-comment 和 prefer-const 規則，以進一步提升開發靈活性。

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -16,7 +16,9 @@ const eslintConfig = [
       "@typescript-eslint/no-explicit-any": "off",
       "@typescript-eslint/no-unused-vars": "off",
       "no-unescaped-entities": "off",
-      "react/no-unescaped-entities": "off"
+      "react/no-unescaped-entities": "off",
+      "@typescript-eslint/ban-ts-comment": "off",
+      "prefer-const": "off"
     }
   }
 ];

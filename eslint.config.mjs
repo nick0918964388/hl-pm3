498d57a034d00db6e9a1d098131328cbdcd229ce
NICKALL-DESKTOP\User <nick0918964388@gmail.com>
2025-05-12 11:24:33 +0800

    更新 ESLint 配置，新增關閉 react/no-unescaped-entities 規則，以進一步提升開發靈活性。

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -15,7 +15,8 @@ const eslintConfig = [
     rules: {
       "@typescript-eslint/no-explicit-any": "off",
       "@typescript-eslint/no-unused-vars": "off",
-      "no-unescaped-entities": "off"
+      "no-unescaped-entities": "off",
+      "react/no-unescaped-entities": "off"
     }
   }
 ];
